a301c0a3a89068473657eaaa5752d4593a1c7da4
Shauna <45226984+ShaunaAyala@users.noreply.github.com>
2024-05-28 09:32:44 -0300

    Add files via upload

---
+++ SFX_Hunter/sounds.lua
@@ -0,0 +1,16 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- Hunter Spells
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccArcane Shot|r", [[Interface\Addons\SFX_Hunter\sound\ArcaneShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccCobra Shot|r", [[Interface\Addons\SFX_Hunter\sound\CobraShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccExplosive Shot|r", [[Interface\Addons\SFX_Hunter\sound\ExplosiveShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccSerpent Sting Shot|r", [[Interface\Addons\SFX_Hunter\sound\SerpentStingShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccMulti Shot|r", [[Interface\Addons\SFX_Hunter\sound\MultiShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccBlack Arrow|r", [[Interface\Addons\SFX_Hunter\sound\BlackArrow.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccKill Shot|r", [[Interface\Addons\SFX_Hunter\sound\KillShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccRapid Fire|r", [[Interface\Addons\SFX_Hunter\sound\RapidFire.ogg]])
+
+-- Example
+-- LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2cc|r Nothing", [[Interface\Addons\SFX_Hunter\sound\Example.ogg]])
+
+
