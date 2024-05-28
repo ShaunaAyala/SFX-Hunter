90341ec2e0f0c7192d6295b4cfede2d045c54ab0
Shauna <45226984+ShaunaAyala@users.noreply.github.com>
2024-05-28 18:24:47 -0300

    Add files via upload

--- SFX_Hunter/sounds.lua
+++ SFX_Hunter/sounds.lua
@@ -9,6 +9,9 @@ LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccMulti Shot|r", [
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccBlack Arrow|r", [[Interface\Addons\SFX_Hunter\sound\BlackArrow.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccKill Shot|r", [[Interface\Addons\SFX_Hunter\sound\KillShot.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccRapid Fire|r", [[Interface\Addons\SFX_Hunter\sound\RapidFire.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccWyvern Sting Shot|r", [[Interface\Addons\SFX_Hunter\sound\WyvernSting.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccTrap Launcher|r", [[Interface\Addons\SFX_Hunter\sound\TrapLauncher.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccMisdirection|r", [[Interface\Addons\SFX_Hunter\sound\Misdirection.ogg]])
 
 -- Example
 -- LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2cc|r Nothing", [[Interface\Addons\SFX_Hunter\sound\Example.ogg]])
