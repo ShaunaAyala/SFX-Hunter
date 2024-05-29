113992cc9232010f1b2c687e4e23c1e8f3159d1d
Shauna <45226984+ShaunaAyala@users.noreply.github.com>
2024-05-28 22:14:46 -0300

    Add files via upload

--- SFX_Hunter/sounds.lua
+++ SFX_Hunter/sounds.lua
@@ -3,17 +3,37 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- Hunter Spells
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccArcane Shot|r", [[Interface\Addons\SFX_Hunter\sound\ArcaneShot.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccCobra Shot|r", [[Interface\Addons\SFX_Hunter\sound\CobraShot.ogg]])
-LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccExplosive Shot|r", [[Interface\Addons\SFX_Hunter\sound\ExplosiveShot.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccSerpent Sting Shot|r", [[Interface\Addons\SFX_Hunter\sound\SerpentStingShot.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccMulti Shot|r", [[Interface\Addons\SFX_Hunter\sound\MultiShot.ogg]])
-LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccBlack Arrow|r", [[Interface\Addons\SFX_Hunter\sound\BlackArrow.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccKill Shot|r", [[Interface\Addons\SFX_Hunter\sound\KillShot.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccRapid Fire|r", [[Interface\Addons\SFX_Hunter\sound\RapidFire.ogg]])
-LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccWyvern Sting Shot|r", [[Interface\Addons\SFX_Hunter\sound\WyvernSting.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccTrap Launcher|r", [[Interface\Addons\SFX_Hunter\sound\TrapLauncher.ogg]])
 LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccMisdirection|r", [[Interface\Addons\SFX_Hunter\sound\Misdirection.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccSteady Shot|r", [[Interface\Addons\SFX_Hunter\sound\SteadyShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccRaptor Strike|r", [[Interface\Addons\SFX_Hunter\sound\RaptorStrike.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccConcussive Shot|r", [[Interface\Addons\SFX_Hunter\sound\ConcussiveShot.ogg]])
+
+-- Hunter Survival
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccExplosive Shot|r", [[Interface\Addons\SFX_Hunter\sound\ExplosiveShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccBlack Arrow|r", [[Interface\Addons\SFX_Hunter\sound\BlackArrow.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccWyvern Sting Shot|r", [[Interface\Addons\SFX_Hunter\sound\WyvernSting.ogg]])
+
+-- Hunter Marksmanship
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccAimed Shot|r", [[Interface\Addons\SFX_Hunter\sound\AimedShot.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccChimera Shot|r", [[Interface\Addons\SFX_Hunter\sound\ChimeraShot.ogg]])
+
+-- Hunter Beast Mastery
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccKill Command|r", [[Interface\Addons\SFX_Hunter\sound\KillCommand.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccIntimidation|r", [[Interface\Addons\SFX_Hunter\sound\Intimidation.ogg]])
+
+-- Random Sounds Generic
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccGeneric 1|r", [[Interface\Addons\SFX_Hunter\sound\Generic1.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccGeneric 2|r", [[Interface\Addons\SFX_Hunter\sound\Generic2.ogg]])
+LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2ccGeneric 3|r", [[Interface\Addons\SFX_Hunter\sound\Generic3.ogg]])
+
+
 
 -- Example
--- LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2cc|r Nothing", [[Interface\Addons\SFX_Hunter\sound\Example.ogg]])
+-- LSM:Register("sound", "|cffF7B731SFX |cffAAD372Hunter |cff99b2cc|r", [[Interface\Addons\SFX_Hunter\sound\Example.ogg]])
 
 
